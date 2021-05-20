ee409b0b460f542892198fedfbd4d18440474be0
masahiro.tsuji <ma.cag08hsp02@gmail.com>
2021-05-20 14:31:26 +0900

    database files

---
+++ src/utils/DBUtil.java
@@ -0,0 +1,28 @@
+package utils;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+// Daoの役割
+public class DBUtil {
+
+    private static final String PERSISTENCE_NAME = "daily_report_management";
+    private static EntityManagerFactory emf; // *１
+
+    public static EntityManager createEntityManager(){
+        return getEntityManagerFactory().createEntityManager();
+    }
+
+    private static EntityManagerFactory getEntityManagerFactory() {
+        if(emf == null){
+            emf = Persistence.createEntityManagerFactory(PERSISTENCE_NAME);
+        }
+        return emf;
+    }
+}
+/*
+ ・Entity : データベースに保管するオブジェクト。データベースレコードに相当
+ *１：EntityManagerがエンティティを受け取り、さまざまなデータベース処理を一括して実行
+
+ */
