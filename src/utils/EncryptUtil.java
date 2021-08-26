082a4fdf8135bd0fc3a7b25c9229a6df9d2b20fd
masahiro-tsuji <57106027+masahiro-tsuji@users.noreply.github.com>
2021-08-26 10:01:20 +0900

    Update EncryptUtil.java

--- src/utils/EncryptUtil.java
+++ src/utils/EncryptUtil.java
@@ -15,11 +15,17 @@ public class EncryptUtil {
             try {
                 bytes = MessageDigest.getInstance("SHA-256").digest(password.getBytes());
                 ret = DatatypeConverter.printHexBinary(bytes);
-            } catch(NoSuchAlgorithmException ex) {}
+            } catch(NoSuchAlgorithmException ex) {} // ある暗号アルゴリズムが要求されたにもかかわらず、現在の環境では使用可能でない場合にスロー
         }
 
         return ret;
     }
 }
+/*
+MessageDigestクラス : SHA-1やSHA-256などのメッセージ・ダイジェスト・アルゴリズムの機能を提供。メッセージ・ダイジェストは、任意サイズのデータを取得して固定長のハッシュ値を出力する安全な一方向のハッシュ機能
+getInstance : 指定されたダイジェスト・アルゴリズムを実装するMessageDigestオブジェクトを返す。
+digest : 最終処理を行ってハッシュ計算を完了します。この呼出しのあと、ダイジェストはリセット(戻り値：結果として得られるハッシュ値に対するバイト・データの配列)
+printHexBinary : バイト配列を文字列に変換
+*/
 // 引数で受け取った文字列にペッパー文字列を連結させたものを SHA256 でハッシュ化
 // 引数の文字列が何もなければ、空の文字列を返す。
